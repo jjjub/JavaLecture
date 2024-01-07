efc33bbf0487cbeafdd3b4db865581786f092ae0
unknown <js4real@naver.com>
2024-01-08 03:40:47 +0900

    과제

--- src/com/test/question/Q034.java
+++ src/com/test/question/Q034.java
@@ -7,7 +7,6 @@ public class Q034 {
 	
 	public static void main(String[] args) {
 			int year, month, date;
-			String result;
 			Scanner sc = new Scanner(System.in);
 			
 			System.out.print("년: ");
@@ -17,15 +16,15 @@ public class Q034 {
 			System.out.print("일: ");
 			date = sc.nextInt();
 
-			Calendar c = Calendar. getInstance();
-			c.set(year, month - 1, date);
+			Calendar c1 = Calendar. getInstance();
+			c1.set(year, month - 1, date);
 			
-			if (c.get (Calendar.DAY_OF_WEEK) != 1 && c.get (Calendar.DAY_OF_WEEK) != 7) {
+			if (c1.get (Calendar.DAY_OF_WEEK) != 1 && c1.get (Calendar.DAY_OF_WEEK) != 7) {
 					System.out.println("입력하신 날짜는 평일입니다.");
 					System.out.println("해당 주의 토요일로 이동합니다.");
 					
-					c.add (Calendar.DATE, 7 - c.get(Calendar.DAY_OF_WEEK));
-					System.out.printf("이동한 날짜는 '%tF'입니다. \n", c);
+					c1.add (Calendar.DATE, 7 - c1.get(Calendar.DAY_OF_WEEK));
+					System.out.printf("이동한 날짜는 '%tF'입니다. \n", c1);
 			} else {
 				System.out.println("입력하신 날짜는 '휴일(토/일)' 입니다.");
 				System.out.println("결과가 없습니다.");
