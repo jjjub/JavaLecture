4f92ee7f2cfec6aa7b8aec79e152302c8302ab29
jjjub <js4real@naver.com>
2024-01-10 17:47:47 +0900

    문자열

--- src/com/test/question/Q065.java
+++ src/com/test/question/Q065.java
@@ -1,19 +1,34 @@
 package com.test.question;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Q065 {
 
 	public static void main(String[] args) {
-		
+
 		int max = 0;
 		int min = 0;
+		int[] og = new int[20];
+		
+		int num;
+
 		Scanner sc = new Scanner(System.in);
 		System.out.print("최대 범위: ");
 		max = sc.nextInt();
 		System.out.print("최소 범위: ");
 		min = sc.nextInt();
+		int[] result = new int[20];
 		
+		for (int i = 0; i < 20; i++) {
+			num = (int) (Math.random() * 20) + 1;	
+			og[i] = num;							//ok
+			if (num <= max && num >= min) {
+				result[i] = num;
+			}
+		}
+		System.out.println("원본: " + Arrays.toString(og));
+		System.out.println("결과: " + Arrays.toString(result));
 
 	}
 
---
+++ src/com/test/question/Q066.java
@@ -0,0 +1,25 @@
+package com.test.question;
+
+public class Q066 {
+
+	public static void main(String[] args) {
+		
+		int[] lotto = new int[6];
+		int count = 0;
+		while(count < 6) {
+			//1~45사이에 난수 발생
+			//만약 이전에 나온 동일한 숫자가 나오면 취소
+			//문제 없으면 count++
+			//lotto배열에 저장
+			
+			int random = (int)(Math.random()*45)+1;
+			for(int i; ; ){
+				
+			}
+				
+			
+		}
+
+	}
+
+}
